ee90b59889727277f53d424c76d705268e3c82f9
figuemon <mfigueroacr@gmail.com>
2017-10-01 21:02:15 -0600

    Added matrix sample file. Include functions to read matrix data from file

--- src/pagerank.erl
+++ src/pagerank.erl
@@ -7,6 +7,11 @@
 
 -module(pagerank).
 
+-compile(export_all).
+
+%%Output folder
+-define(FOLDER, "output/").
+
 %% > multMatriz(Nodos, Trabajadores, Reductores, Matrix, Vector).
 %% Donde:
 %% 1. Nodos es la lista de nombres de los nodos donde va a ejecutar los trabajadores.
@@ -16,5 +21,35 @@
 %% nodos.
 %% 4. Matrix es el nombre del archivo donde se encuentra la matriz.
 %% 5. Vector es el nombre del archivo donde se encuentra el vector a multiplicar.
+multMatriz(Nodos, Trabajadores, Reductores, Matrix, Vector)->
+	%TODO Create outputFolder
+	blocks = readMatrixFile(Matrix).
+
+%%================================================
+%% Procesamiento del archivo de Matriz
+%%================================================
+
+readMatrixFile(File) ->
+    {ok, Device} = file:open(File,[read]),   
+    readMatrixLines(Device, 1, sets:new()).
+
+readMatrixLines(Device, S, Workers) ->
+    case io:get_line(Device, "") of
+        eof  -> file:close(Device), sets:to_list(Workers);
+		Line -> io:format("Dato: ~s ", [Line]),
+        readMatrixLines(Device, S + 1, Workers)
+	end.
+		
 
 
+
+%%================================================
+%% Funcion Map
+%%================================================
+map_function({Min, Max, List}, Reduce_PID) ->
+	link(Reduce_PID), % If the reduce process exits early, extra map processes will also end.
+	Result = [A * B * C || 	A <- List,
+				B <- List,
+				C <- lists:seq(Min, Max),
+				A*A + B*B == C*C andalso A + B + C == 1000],
+	Reduce_PID ! Result.
